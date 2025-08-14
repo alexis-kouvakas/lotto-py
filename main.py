c77164df2b2cd3bd1fa3310677aff0f07b99bd24
alexis kouvakas <alexios.kouvakas@proton.me>
2025-08-14 23:36:20 +0200

    Compacted loop function

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import random
 winning_numbers = set(random.sample(range(1, 49), 6))
 
 print('Guesses must be integer numbers between 1 and 49.')
-loop  = 7
 guesses = set()
-for x in range(1, loop):
+for x in range(1, 7):
  x = x+1
  guesses.add(int(input('Pick your guess: ')))
 
 matches = winning_numbers.intersection(guesses)
-
-score = len(matches)
-
 print('The winning numbers were', *winning_numbers,'.')
-print('You had', score, 'matches!')
+print('You had', len(matches) , 'matches!')
 if len(matches) > 0: print('They were', *matches,'!')
 
