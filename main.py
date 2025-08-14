52b2ba3ed12efb9ca88047264600ed4efa4e43fd
alexis kouvakas <alexios.kouvakas@proton.me>
2025-08-14 23:10:47 +0200

    Refactor lottery game to use loop for guesses and improved output
    - Replaced multiple input statements with a for loop to collect guesses.
    - Added upfront message reminding users that guesses must be between 1 and 49.
    - Improved output formatting:
      - Display winning numbers as a space-separated list.
      - Show matched numbers when there is at least one match.
    - Simplified variable initialization by removing redundant code.

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import random
 
 winning_numbers = set(random.sample(range(1, 49), 6))
 
-guesses = set() 
-
-guesses.add(int(input('Pick your first guess! Must be an integer number between 1 and 49: ')))
-guesses.add(int(input('Pick your second guess! Must be an integer number between 1 and 49: ')))
-guesses.add(int(input('Pick your third guess! Must be an integer number between 1 and 49: ')))
-guesses.add(int(input('Pick your fourth guess! Must be an integer number between 1 and 49: ')))
-guesses.add(int(input('Pick your fifth guess! Must be an integer number between 1 and 49: ')))
-guesses.add(int(input('Pick your sixth guess! Must be an integer number between 1 and 49: ')))
+print('Guesses must be integer numbers between 1 and 49.')
+loop  = 7
+guesses = set()
+for x in range(1, loop):
+ x = x+1
+ guesses.add(int(input('Pick your guess: ')))
 
 matches = winning_numbers.intersection(guesses)
 
 score = len(matches)
 
-print('The winning numbers were', winning_numbers,'.')
+print('The winning numbers were', *winning_numbers,'.')
 print('You had', score, 'matches!')
+if len(matches) > 0: print('They were', *matches,'!')
+
