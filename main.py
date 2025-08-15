80dde7cbfe711a4abe63a41a7fdb4ac939c0ed34
alexis kouvakas <alexios.kouvakas@proton.me>
2025-08-15 12:40:53 +0200

    Added recursive input function with validation step
    - Added validation to ensure guesses are integers between 1 and 49.
    - Provided feedback when invalid input is entered.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import random
 
+def guess_input(guesses, attempts):
+ if  attempts == 0: 
+  return guesses
+ guess = int(input('Pick your guess: '))
+ if not (1 <= guess <= 49):
+  print('Invalid input')
+  guess_input(guesses, attempts)
+ else:
+  guesses.add(guess)
+  guess_input(guesses, attempts-1)
+
 winning_numbers = set(random.sample(range(1, 49), 6))
-
-print('Guesses must be integer numbers between 1 and 49.')
+print('Guesses must be integer numbers between 1 and 49.') 
 guesses = set()
-for x in range(1, 7):
- x = x+1
- guesses.add(int(input('Pick your guess: ')))
-
+attempts=6
+guess_input(guesses, attempts)
+    
+     
 matches = winning_numbers.intersection(guesses)
 print('The winning numbers were', *winning_numbers,'.')
 print('You had', len(matches) , 'matches!')
